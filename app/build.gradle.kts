cc7380a1d3088fcdb0c8ed2c63bfdf97d077443d
Akshay Komar <akshaykomar732@gmail.com>
2024-08-25 20:32:53 -0700

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,4 +114,9 @@ dependencies {
     implementation (libs.converter.gson)
     implementation(libs.converter.scalars)
 
+
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+
 }
